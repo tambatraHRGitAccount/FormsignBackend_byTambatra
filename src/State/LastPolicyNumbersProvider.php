610ab4f90174f95dd5d53fdb5d19212ab63c02bc
michaeltserika <mitsetra@gmail.com>
2025-05-22 16:31:38 +0300

    LastPolicyNumberProvider

--- src/State/LastPolicyNumbersProvider.php
+++ src/State/LastPolicyNumbersProvider.php
@@ -25,11 +25,11 @@ class LastPolicyNumbersProvider implements ProviderInterface
         $dto = new LastPolicyNumbersDto();
         if ($lastPolicy) {
             $dto->placingNumber = $lastPolicy->getPlacingNumber() ?? '3000';
-            $dto->qbInvNum = $lastPolicy->getQbInvNum() ?? '20000';
+            $dto->qbInvNum = $lastPolicy->getQbInvNum() ?? '23135';
             $dto->policy = $lastPolicy->getPolicy() ?? '10000';
         } else {
             $dto->placingNumber = '3000';
-            $dto->qbInvNum = '20000';
+            $dto->qbInvNum = '23135';
             $dto->policy = '10000';
         }
 
